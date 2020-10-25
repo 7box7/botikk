b9634483585205464a1d42eebca3d55bb87a1180
7box7 <60572459+7box7@users.noreply.github.com>
2020-10-26 01:28:29 +0300

    Create vk_bot2.py

---
+++ vk_bot2.py
@@ -0,0 +1,22 @@
+from time import time, sleep
+import vk_api
+import os
+
+tok = str(os.environ.get('VK_KEY'))
+tok2 = str(os.environ.get('VK_KEY2'))
+
+vk = vk_api.VkApi(token=tok2)
+vk_group = vk_api.VkApi(token=tok)
+
+
+while True:
+    sleep(2)
+    try:
+        g = vk.method('status.get', {'user_id': '227843991'})
+        b = g["text"]
+        alll = vk_group.method('messages.search', {'peer_id': '393598407', 'q': '.', 'count': 1})['items'][0]['text']
+        if alll == b or b == "":
+            continue
+        g = vk_group.method('messages.send', {'user_id': '393598407', 'message': b, 'random_id': int(time())})
+    except:
+        continue
