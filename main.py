ec53d513b70a72815d28d1d0ae8a40b6b244496b
7box7 <60572459+7box7@users.noreply.github.com>
2021-12-30 16:42:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from time import time, sleep
+import vk_api
+import os
+
+tok = str(os.environ.get('VK1'))
+tok2 = str(os.environ.get('VK2'))
+
+vk = vk_api.VkApi(token=tok)
+vk_group = vk_api.VkApi(token=tok2)
+
+while True:
+    sleep(2)
+    g = vk.method('status.get', {'user_id': '227843991'})
+    b = g["text"]
+    alll = vk_group.method('messages.search', {'peer_id': '393598407', 'q': '.', 'count': 1})['items'][0]['text']
+    if alll == b or b == "":
+        continue
+    g = vk_group.method('messages.send', {'user_id': '393598407', 'message': b, 'random_id': int(time())})
+    continue
