4b0ea164db743391b7cf71de0c75b48c25a06139
7box7 <60572459+7box7@users.noreply.github.com>
2021-12-30 16:44:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ tok2 = str(os.environ.get('VK2'))
 vk = vk_api.VkApi(token=tok)
 vk_group = vk_api.VkApi(token=tok2)
 
+#493552486
+#227843991
+
 while True:
     sleep(2)
-    g = vk.method('status.get', {'user_id': '227843991'})
+    g = vk.method('status.get', {'user_id': '493552486'})
     b = g["text"]
     alll = vk_group.method('messages.search', {'peer_id': '393598407', 'q': '.', 'count': 1})['items'][0]['text']
     if alll == b or b == "":
